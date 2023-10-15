15817badd9d2badb163531bd0995fab05afbfb08
darius <dariusjosedelacruz@gmail.com>
2023-10-15 17:52:54 -0400

    new

--- process_automation/settings.py
+++ process_automation/settings.py
@@ -18,9 +18,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
 
-ALLOWED_HOSTS = [
-    'www.app-ucsd.works',
-]
+ALLOWED_HOSTS = ['*']
+
+
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
