96e1b838a51c0a7b607a35bd6e7369849cae1bc1
darius <dariusjosedelacruz@gmail.com>
2023-10-15 21:56:07 -0400

    new

--- catalog/admin/subject_student_admin.py
+++ catalog/admin/subject_student_admin.py
@@ -3,7 +3,7 @@ from catalog.models import SubjectStudentModel
 
 class SubjectStudentModelAdmin(admin.ModelAdmin):
     list_display = ('subject', 'student', 'status', 'created', 'modified')
-    list_filter = ('status', 'subject__code', 'student__user__first_name', 'student__user__last_name')
+    list_filter = ('status', 'subject__code', 'student__student_id','student__user__first_name', 'student__user__last_name')
     search_fields = ('subject__code', 'student__user__first_name', 'student__user__last_name', 'status')
     list_per_page = 20
 
