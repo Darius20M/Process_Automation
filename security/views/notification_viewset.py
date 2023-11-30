15542b3ac18ecbf0e4c75f15228dac3b657762a4
darius <dariusjosedelacruz@gmail.com>
2023-11-29 23:58:39 -0400

    new

--- security/views/notification_viewset.py
+++ security/views/notification_viewset.py
@@ -1,6 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
+from accounts.filter import StudentProfileFilter
 from security.models import NotificationModel
 from security.serializers import NotificationSerializer
 
@@ -12,5 +15,6 @@ class NotificationViewSet(ModelViewSet):
     serializer_class = NotificationSerializer
     queryset = NotificationModel.objects.all()
     ordering_fields = ('id', 'created')
+    filter_backends = (StudentProfileFilter, DjangoFilterBackend, SearchFilter, OrderingFilter,)
     filterset_fields = ('user','request_n', 'level', 'unread', 'created',)
     search_fields = ('title', 'user__first_name', 'user__username', 'user__email', 'user__last_name', 'message',)
